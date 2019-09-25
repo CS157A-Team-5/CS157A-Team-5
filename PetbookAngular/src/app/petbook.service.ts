041f66d73d33664b146f2f3a9f595782258fd62b
Elana Olson <elanaolson@gmail.com>
2019-09-24 18:39:53 -0700

    connect service and fix api url

--- PetbookAngular/src/app/petbook.service.ts
+++ PetbookAngular/src/app/petbook.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class PetbookService { 
 	constructor(private http: HttpClient, private router: Router) { }
-	url = 'http://localhost:3000';
-	getPets() {
-		return this.http.get('${this.url}/pets');
+	url = 'http://localhost:3000/pets';
+	
+	getPets(): Observable<Object> {
+		return this.http.get(this.url);
 	}
+	
 	createPet(data) {
-		this.http.post('${this.url}/pets', data).subscribe(
+		this.http.post(this.url, data).subscribe(
 			res => {
 				console.log(res);
 				this.router.navigateByUrl('/pet');
--- PetbookAngular/src/app/petbook/petbook.component.ts
+++ PetbookAngular/src/app/petbook/petbook.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { PetbookService } from '../petbook.service';
 
 @Component({
   selector: 'app-petbook',
   templateUrl: './petbook.component.html',
-  styleUrls: ['./petbook.component.css']
+  styleUrls: ['./petbook.component.css'],
+  providers: [PetbookService],
 })
 export class PetbookComponent implements OnInit {
+  data;
 
-  constructor() { }
+  constructor(private petbookService: PetbookService) { 
+    this.data = petbookService.getPets();
+  }
 
   ngOnInit() {
   }
