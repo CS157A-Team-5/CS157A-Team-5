8f9c373df4d7bc18dd10674e5e619f1a82d94d78
Lovejit <kharodl@gmail.com>
2019-09-26 15:25:13 -0700

    Refreshing after creation/update

--- PetBookNode/petbook/Petbook.js
+++ PetBookNode/petbook/Petbook.js
@@ -4,14 +4,14 @@ var Petbook = {
     getpets: function(callback) {
         return db.query('SELECT * FROM t_pets', callback);
     },
-    createpet: function (value, callback) {
+    createpet: function (pet, callback) {
         return db.query('INSERT INTO t_pets(name, species, age) VALUES(?, ?, ?)', [pet.name, pet.species, pet.age], callback);
     },
     updatepet: function (pet, callback) {
     	return db.query('UPDATE t_pets SET name=?, species=?, age=? WHERE ID=?', [pet.name, pet.species, pet.age, pet.id], callback);
     },
     deletepet: function (pet, callback) {
-    	return db.query('DELETE FROM t_pets WHERE ID=?', [pet.id], callback);
+    	return db.query('DELETE FROM t_pets WHERE ID=34', [pet.id], callback);
     }
 }
 
