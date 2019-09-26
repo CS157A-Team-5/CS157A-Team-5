09b866b971cb6861bcbf1a7af8f2eb46718e87ae
Lovejit <kharodl@gmail.com>
2019-09-26 15:12:55 -0700

    Name change for readability

--- PetBookNode/petbook/Petbook.js
+++ PetBookNode/petbook/Petbook.js
@@ -4,14 +4,14 @@ var Petbook = {
     getpets: function(callback) {
         return db.query('SELECT * FROM t_pets', callback);
     },
-    createpet: function (Petbook, callback) {
-        return db.query('INSERT INTO t_pets(name, species, age) VALUES(?, ?, ?)', [Petbook.name, Petbook.species, Petbook.age], callback);
+    createpet: function (pet, callback) {
+        return db.query('INSERT INTO t_pets(name, species, age) VALUES(?, ?, ?)', [pet.name, pet.species, pet.age], callback);
     },
-    updatepet: function (Petbook, callback) {
-    	return db.query('UPDATE t_pets SET name=?, species=? WHERE ID=?', [Petbook.name, Petbook.species, Petbook.id], callback);
+    updatepet: function (pet, callback) {
+    	return db.query('UPDATE t_pets SET name=?, species=? WHERE ID=?', [pet.name, pet.species, pet.id], callback);
     },
-    deletepet: function (Petbook, callback) {
-    	return db.query('DELETE FROM t_pets WHERE ID=?', [Petbook.id], callback);
+    deletepet: function (pet, callback) {
+    	return db.query('DELETE FROM t_pets WHERE ID=?', [pet.id], callback);
     }
 }
 
