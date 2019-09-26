f97c81a474d126ba34d52c3cd2d07efbcaad9f78
Lovejit <kharodl@gmail.com>
2019-09-26 14:24:39 -0700

    Added the rest of CRUD functionality on Node.js middleware

--- PetBookNode/petbook/Petbook.js
+++ PetBookNode/petbook/Petbook.js
@@ -2,10 +2,16 @@ var db = require('../db');
 
 var Petbook = {
     getpets: function(callback) {
-        return db.query('SELECT * from t_pets', callback);
+        return db.query('SELECT * FROM t_pets', callback);
     },
     createpet: function (Petbook, callback) {
-        return db.query('Insert into t_pets(name, species) values(?, ?)',[Petbook.name, Petbook.species], callback);
+        return db.query('INSERT INTO t_pets(name, species, age) VALUES(?, ?, ?)', [Petbook.name, Petbook.species, Petbook.age], callback);
+    },
+    updatepet: function (Petbook, callback) {
+    	return db.query('UPDATE t_pets SET name=?, species=? WHERE ID=?', [Petbook.name, Petbook.species, Petbook.id], callback);
+    },
+    deletepet: function (Petbook, callback) {
+    	return db.query('DELETE FROM t_pets WHERE ID=?', [Petbook.id], callback);
     }
 }
 
