1f854d3e3404f56b23b082b1faceb3b5c479eea4
Elana Olson <elanaolson@gmail.com>
2019-12-01 16:43:14 -0800

    profile page (#25)
    * async use pets data and mat-card display
    
    * get current user profile

--- PetbookAngular/src/app/petbook/petbook.component.ts
+++ PetbookAngular/src/app/petbook/petbook.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PetbookService } from '../petbook.service';
 import { Pet } from '../petbook.interface';
+import { forkJoin, combineLatest, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-petbook',
@@ -8,36 +9,54 @@ import { Pet } from '../petbook.interface';
   styleUrls: ['./petbook.component.css'],
   providers: [PetbookService],
 })
-export class PetbookComponent implements OnInit {
+export class PetbookComponent {
   pets: Pet[];
+  friends: Observable<any[]>;
+  clubs: Observable<any[]>;
+  currentUserID;
 
-  constructor(private petbookService: PetbookService) { 
+  constructor(private petbookService: PetbookService) {
+    this.currentUserID = +this.petbookService.getCurrentStorageStatus();
     this.getPets();
   }
 
-  ngOnInit() {
+  getPets() {
+    this.petbookService.getPetsByOwner(this.currentUserID)
+      .subscribe((res: Pet[]) => {
+        this.pets = res;
+        console.log(this.pets);
+        this.getFriends();
+        this.getClubs();
+      });
   }
 
-  getPets() {
-  	this.petbookService.getPetsByName('Annie').subscribe((res: Pet[])=>{
-    	this.pets = res;
-		console.log(this.pets);
-    });
+  getFriends() {
+    const queryFriendsResults = [];
+    for (const pet of this.pets) {
+      queryFriendsResults.push(this.petbookService.getFriendsByPet(pet.id));
+    }
+
+    this.friends = combineLatest(queryFriendsResults);
+  }
+
+  getClubs() {
+    this.clubs = this.petbookService.getClubsByOwner(this.currentUserID);
   }
 
   createPet(model: Pet) {
-  	console.log(model);
-  	this.petbookService.createPet(model);
+    console.log(model);
+    model.owner_id = this.currentUserID;
+    this.petbookService.createPet(model);
   }
 
   updatePet(model: Pet) {
-  	console.log(model);
-  	this.petbookService.updatePet(model);
+    console.log(model);
+    this.petbookService.updatePet(model);
   }
 
   deletePet(model: Pet) {
-  	console.log(model);
-  	this.petbookService.deletePet(100);
+    console.log(model);
+    this.petbookService.deletePet(100);
   }
 
 }
