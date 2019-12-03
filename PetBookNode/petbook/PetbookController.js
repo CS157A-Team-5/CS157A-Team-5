0e9a15c70ebf0599adbc6f7c88434038f07e5755
Lovejit <kharodl@gmail.com>
2019-12-02 18:52:38 -0800

    Last typo

--- PetBookNode/petbook/PetbookController.js
+++ PetBookNode/petbook/PetbookController.js
@@ -38,7 +38,7 @@ router.get('/user/login', function (req, res) {
 });
 
 router.get('/user/location/:owner_id', function (req, res) {
-    Petbook.getuserlocation(req.params.owner_id, function(err, rows) {
+    Petbook.getownerlocation(req.params.owner_id, function(err, rows) {
         if(err) {
             res.status(400).json(err);
         } else {
